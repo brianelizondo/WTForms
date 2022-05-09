1dce8a1fecd188f5926305c73c6d44c1f14aacab
Brian Elizondo <brian_elizondo@msn.com>
2022-05-09 17:38:22 -0400

    Step 2: Make Homepage Listing Pets => The homepage list the pets with name, photo and display if the pet is available

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from crypt import methods
 from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from sqlalchemy import desc
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "abcd1234"
@@ -14,3 +15,15 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 # db.create_all()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Custom 404 Error Page"""
+    return render_template('404.html'), 404
+
+@app.route("/")
+def home_page():
+    """Homepage """
+
+    pets = Pet.query.order_by(desc(Pet.id)).all()
+    return render_template("home.html", pets=pets)
