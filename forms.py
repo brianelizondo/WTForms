0c6c09bc766b2aea8521d90190bdb49d90a841d3
Brian Elizondo <brian_elizondo@msn.com>
2022-05-09 20:49:40 -0400

    Step 5: Add Validation => Use the WTForms validator for validating the fields species, photo URL and age

--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ class AddPetForm(FlaskForm):
     """Form for adding Pets"""
 
     name = StringField("Pet Name", validators=[InputRequired(message='Please enter the Pet Name')])
-    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine'), ('pork', 'Pork'), ('rabbit', 'Rabbit')])
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
     photo_url = URLField("Photo URL", validators=[Optional(), URL(message='Please enter a valid URL')])
-    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=100, message='Please enter an Age between 0 - 100')])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30, message='Please enter an Age between 0 - 30')])
     notes = TextAreaField("Notes", validators=[Optional()])
     
